2892aa548d9bfccaead36cd2fc3fd229bf31e978
Matt Sullivan <matt.sullivan@sbnation.com>
2017-10-09 11:48:32 -0400

    Mobile updates

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,3 +1,5 @@
+// shows nave after landing page is scrolled
+
 $(function() {
 
     var header = $("#c-header");
@@ -16,8 +18,30 @@ $(function() {
     });
 });
 
+// color randomizer
+
 $(function() {
     var classes = ["blue", "pink", "gold", "green", "red", "orange", "purple"];
 
     $('body').addClass(classes[~~(Math.random()*classes.length)]);
 });
+
+// smooth scroll
+
+$(function() {
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
--- index.md
+++ index.md
@@ -1,4 +1,28 @@
 ---
 layout: home
 ---
+<div class="c-post--intro">
+  <div class="c-beat-logo">
+    {% include svgs/vox-logo.html %}
+    {% include svgs/beat-logo.html %}
+  </div>
 
+  <div class="c-seal">
+    {% include svgs/seal.html %}
+  </div>
+
+  <div class="c-about-main">
+    <a href="">sign up</a>
+    {% for post in site.posts reversed %}
+      <a class="nav-items" href="#{{ post.title | escape }}">{{ post.title | escape }}</a>
+    {% endfor %}
+  </div>
+
+  <div class="c-post-tag-left">
+    VOX PRODUCT
+  </div>
+
+  <div class="c-post-tag-right">
+    JANUARY 2017
+  </div>
+</div>
